c94b266819ec200058880422c7dfb2fd81fac2d7
nicsjocson <hnjocson@gmail.com>
2026-04-06 15:19:02 +0800

    models and urls code

--- tapas/tapasapp/models.py
+++ tapas/tapasapp/models.py
@@ -11,3 +11,15 @@ class Dish(models.Model):
     def __str__(self):
         return str(self.pk) + ": " + self.name
     
+class Account(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+
+    def getUsername(self):
+        return self.username
+
+    def getPassword(self):
+        return self.password
+
+    def __str__(self):
+        return self.username
--- tapas/tapasapp/urls.py
+++ tapas/tapasapp/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.better_menu, name='better_menu'),
+    path('', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
+    path('basic_list/<int:pk>/', views.basic_list_view, name='basic_list'),
+    path('manage_account/<int:pk>/', views.manage_account_view, name='manage_account'),
+    path('change_password/<int:pk>/', views.change_password_view, name='change_password'),
+    path('delete_account/<int:pk>/', views.delete_account_view, name='delete_account'),
+    
+    path('menu/', views.better_menu, name='better_menu'),
     path('add_menu', views.add_menu, name='add_menu'),
     path('view_detail/<int:pk>/', views.view_detail, name='view_detail'),
     path('delete_dish/<int:pk>/', views.delete_dish, name='delete_dish'),
